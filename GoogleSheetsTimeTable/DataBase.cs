f8e97b35baa0ff03c5fddd56d392c9e48fbcc4ef
Frizer <palebedev_1@edu.hse.ru>
2022-09-27 09:25:17 +0300

    TryingCommit

---
+++ GoogleSheetsTimeTable/DataBase.cs
@@ -0,0 +1,30 @@
+namespace SheetsController;
+
+public static class DataBase
+{
+    private const string PathBegin = "../../../../ConsoleApp1/DataBase/";
+
+    public static List<PlayZone> Zones
+    {
+        get
+        {
+            List<PlayZone> result = new();
+            var lines = File.ReadAllLines(PathBegin + "Zones.txt");
+            Console.WriteLine(lines[0]);
+            foreach (var line in lines)
+            {
+                var name = line.Split(":")[0];
+                Console.WriteLine(name);
+                var capacityStr = line.Split(":")[1];
+                int capacity;
+                int.TryParse(capacityStr, out capacity);
+                result.Add(new PlayZone(name, capacity));
+            }
+
+            return result;
+        }
+    }
+
+    public static string SpreadSheetId =>
+        "1PQTy_fIQSOaw2RQoDwkY2F_TCRdPxbQ3E64jclb-Av0"; //File.ReadAllText(PathBegin + "SpreadSheetId.txt");
+}
